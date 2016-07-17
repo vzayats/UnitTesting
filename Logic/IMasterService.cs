69b54a2e4cc77aa9057d775efa3a626818dd50c8
vzayats <vzayats3@gmail.com>
2016-07-17 21:58:19 +0300

    Added solution

---
+++ Logic/IMasterService.cs
@@ -0,0 +1,9 @@
+namespace Logic
+{
+    public interface IMasterService
+    {
+        int GetDoubleSum();
+        double GetAverage();
+        double GetMaxSquare();
+    }
+}
---
+++ Logic/MasterService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Logic
+{
+    public class MasterService : IMasterService
+    {
+        private readonly IAlgoService _algoService;
+
+        private readonly IDataService _dataService;
+        
+        public MasterService(IAlgoService algo, IDataService data)
+        {
+            _algoService = algo;
+            _dataService = data;
+        }
+
+        public int GetDoubleSum()
+        {
+            var data = _dataService.GetAllData();
+            if (data == null || !data.Any())
+            {
+                throw new InvalidOperationException("We have no data to process");
+            }
+
+            return _algoService.DoubleSum(data);;
+        }
+
+        //TODO: Make more methods
+
+        public double GetAverage()
+        {
+            var data = _dataService.GetAllData();
+            return _algoService.GetAverage(data);
+        }
+
+        public double GetMaxSquare()
+        {
+            var data = _dataService.GetMax();
+            return _algoService.Sqr(data);
+        }
+    }
+}
